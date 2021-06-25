4dea12c858d30cf57017deba1b24f825d14df205
rakhi <ramakrishna060997@gmail.com>
2021-06-25 16:45:50 +0530

    checkpoint 3:add few dynamic routes in our application

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express')
+  ,http = require('http')
+  ,fs = require('fs')
+  ,path = require('path');
+
+var app = express();
+
+
+app.get('/movie',function(req,res){
+    res.send("All Movies Data in JSON format from Mongo DB")
+})
+
+app.get('/genres',function(req,res){
+    res.send("All Genres Data in JSON format from Mongo DB")
+})
+
+app.get('/artists',function(req,res){
+    res.send("All Artists Data in JSON format from Mongo DB")
+})
+
+
+
+app.listen(9000, function () {
+    console.log("express has started on port 3000");
+   });
