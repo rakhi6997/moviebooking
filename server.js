555afa988cae76bf05173a740df4eaa17a9dba9a
rakhi <ramakrishna060997@gmail.com>
2021-06-26 18:07:31 +0530

    Checkpoint 5:Define the schema,Set up the index file

--- server.js
+++ server.js
@@ -4,7 +4,20 @@ var express = require('express')
   ,path = require('path');
 
 var app = express();
+const db = require("./models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
 
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
 
 app.get('/movie',function(req,res){
     res.send("All Movies Data in JSON format from Mongo DB")
